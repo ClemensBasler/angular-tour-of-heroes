d8a7beba2c7904d47e8b6bfc1725cb23a07fb32e
Clemens Basler <clemens-basler@t-online.de>
2017-05-31 21:54:25 +0200

    working on pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,16 @@ export class Hero {
 }
 
 const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice', hp: 30, rs: 1, ini: 15 },
-  { id: 12, name: 'Narco', hp: 30, rs: 1, ini: 15 },
-  { id: 13, name: 'Bombasto', hp: 30, rs: 1, ini: 15 },
-  { id: 14, name: 'Celeritas', hp: 30, rs: 1, ini: 15 },
-  { id: 15, name: 'Magneta', hp: 30, rs: 1, ini: 15 },
-  { id: 16, name: 'RubberMan', hp: 30, rs: 1, ini: 15 },
-  { id: 17, name: 'Dynama', hp: 30, rs: 1, ini: 15 },
-  { id: 18, name: 'Dr IQ', hp: 30, rs: 1, ini: 15 },
-  { id: 19, name: 'Magma', hp: 30, rs: 1, ini: 15 },
-  { id: 20, name: 'Tornado', hp: 30, rs: 1, ini: 15 }
+  { id: 11, name: 'Mr. Nice', hp: 30, rs: 1, ini: 10 },
+  { id: 12, name: 'Narco', hp: 30, rs: 1, ini: 9 },
+  { id: 13, name: 'Bombasto', hp: 30, rs: 1, ini: 5 },
+  { id: 14, name: 'Celeritas', hp: 30, rs: 1, ini: 2 },
+  { id: 15, name: 'Magneta', hp: 30, rs: 1, ini: 20 },
+  { id: 16, name: 'RubberMan', hp: 30, rs: 1, ini: 11 },
+  { id: 17, name: 'Dynama', hp: 30, rs: 1, ini: 200 },
+  { id: 18, name: 'Dr IQ', hp: 30, rs: 1, ini: 123 },
+  { id: 19, name: 'Magma', hp: 30, rs: 1, ini: 123 },
+  { id: 20, name: 'Tornado', hp: 30, rs: 1, ini: 1233 }
 ];
 
 @Component({
