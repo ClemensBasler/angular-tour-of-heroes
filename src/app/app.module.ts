35e80da985cda1b3d5bd819712053504396775be
Clemens Basler <clemens-basler@t-online.de>
2017-05-31 22:23:50 +0200

    added orderB< pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import {FilterArrayPipe} from './pipes/ini.pipe';
+import {OrderByPipe} from './pipes/ini.pipe';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FilterArrayPipe
+    OrderByPipe
   ],
   imports: [
     BrowserModule,
