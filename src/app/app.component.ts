9b9b9c4cc2485443193390c4ef543e0a858beb97
Clemens Basler <clemens-basler@t-online.de>
2017-06-06 12:30:51 +0200

    damage works!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,19 +5,20 @@ export class Hero {
   hp: number;
   rs: number;
   ini: number;
+  dead: boolean;
 }
 
 const HEROES: Hero[] = [
-  { name: 'Mr. Nice', hp: 30, rs: 1, ini: 20 },
-  { name: 'Narco', hp: 30, rs: 1, ini: 20 },
-  { name: 'Bombasto', hp: 30, rs: 1, ini: 20 },
-  { name: 'Celeritas', hp: 30, rs: 1, ini: 20 },
-  { name: 'Magneta', hp: 30, rs: 1, ini: 21 },
-  { name: 'RubberMan', hp: 30, rs: 1, ini: 10 },
-  { name: 'Dynama', hp: 30, rs: 1, ini: 10 },
-  { name: 'Dr IQ', hp: 30, rs: 1, ini: 10 },
-  { name: 'Magma', hp: 30, rs: 1, ini: 20 },
-  { name: 'Tornado', hp: 30, rs: 1, ini: 20 }
+  { name: 'Mr. Nice', hp: 30, rs: 1, ini: 20, dead: false },
+  { name: 'Narco', hp: 30, rs: 1, ini: 20, dead: false },
+  { name: 'Bombasto', hp: 30, rs: 1, ini: 20, dead: false },
+  { name: 'Celeritas', hp: 30, rs: 1, ini: 20, dead: false },
+  { name: 'Magneta', hp: 30, rs: 1, ini: 21, dead: false },
+  { name: 'RubberMan', hp: 30, rs: 1, ini: 10, dead: false },
+  { name: 'Dynama', hp: 30, rs: 1, ini: 10, dead: false },
+  { name: 'Dr IQ', hp: 30, rs: 1, ini: 10, dead: false },
+  { name: 'Magma', hp: 30, rs: 1, ini: 20, dead: false },
+  { name: 'Tornado', hp: 30, rs: 1, ini: 20, dead: false }
 ];
 
 @Component({
@@ -32,8 +33,11 @@ export class AppComponent {
   heroes = HEROES;
   selectedHero: Hero;
   onTurn = Hero[0];
+  test = 1;
+  isVisible = false;
 
   onSelect(hero: Hero): void {
+    this.test = hero.hp;
     this.selectedHero = hero;
   }
 
@@ -41,5 +45,9 @@ export class AppComponent {
     if (damage > this.selectedHero.rs) {
         this.selectedHero.hp -= (damage - this.selectedHero.rs);
     }
+    if(this.selectedHero.hp < 0){
+      this.selectedHero.dead = true;
+    }
+    this.damage = 0;
   }
 }
