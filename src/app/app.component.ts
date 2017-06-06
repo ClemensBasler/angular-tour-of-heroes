0961de5902c56465aca59e2401ef3e91ac29cffd
Clemens Basler <clemens-basler@t-online.de>
2017-06-06 13:46:36 +0200

    aventurien style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,15 @@ export class Hero {
 }
 
 const HEROES: Hero[] = [
-  { name: 'Mr. Nice', hp: 30, rs: 1, ini: 20, dead: false },
-  { name: 'Narco', hp: 30, rs: 1, ini: 20, dead: false },
-  { name: 'Bombasto', hp: 30, rs: 1, ini: 20, dead: false },
-  { name: 'Celeritas', hp: 30, rs: 1, ini: 20, dead: false },
-  { name: 'Magneta', hp: 30, rs: 1, ini: 21, dead: false },
-  { name: 'RubberMan', hp: 30, rs: 1, ini: 10, dead: false },
-  { name: 'Dynama', hp: 30, rs: 1, ini: 10, dead: false },
-  { name: 'Dr IQ', hp: 30, rs: 1, ini: 10, dead: false },
-  { name: 'Magma', hp: 30, rs: 1, ini: 20, dead: false },
-  { name: 'Tornado', hp: 30, rs: 1, ini: 20, dead: false }
+  { name: 'Schlange 1', hp: 30, rs: 1, ini: 20, dead: false },
+  { name: 'Schlange 2', hp: 30, rs: 1, ini: 20, dead: false },
+  { name: 'Liscom', hp: 30, rs: 1, ini: 20, dead: false },
+  { name: 'Borbarad', hp: 30, rs: 1, ini: 20, dead: false },
+  { name: 'Skelett 1', hp: 30, rs: 1, ini: 21, dead: false },
+  { name: 'Eridon', hp: 30, rs: 1, ini: 10, dead: false },
+  { name: 'Akja', hp: 30, rs: 1, ini: 10, dead: false },
+  { name: 'Tharg', hp: 30, rs: 1, ini: 10, dead: false },
+  { name: 'Borbarads Mudda', hp: 30, rs: 1, ini: 20, dead: false }
 ];
 
 @Component({
@@ -33,11 +32,9 @@ export class AppComponent {
   heroes = HEROES;
   selectedHero: Hero;
   onTurn = Hero[0];
-  test = 1;
   isVisible = false;
 
   onSelect(hero: Hero): void {
-    this.test = hero.hp;
     this.selectedHero = hero;
   }
 
