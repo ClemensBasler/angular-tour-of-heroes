b0e827bbb540df96891d8cf4bf84429bf7f3aea3
Clemens Basler <clemens-basler@t-online.de>
2017-06-07 19:43:27 +0200

    css stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,22 @@ export class Hero {
   rs: number;
   ini: number;
   dead: boolean;
+  mr1: number;
+  mr2: number;
 }
 
-// AT, PA, TP, GS, MR/MR, beondere Kampfregeln
+// AT, PA, TP, GS, MR/MR, beondere Kampfregeln, Counter, neue Kampfrunde, wer ist dran?, Bogen/Armbrust
+// Stati farblich => Wunden
 const HEROES: Hero[] = [
-  { name: 'Schlange 1', hp: 30, rs: 1, ini: 20, dead: false },
-  { name: 'Schlange 2', hp: 30, rs: 1, ini: 20, dead: false },
-  { name: 'Liscom', hp: 30, rs: 1, ini: 20, dead: false },
-  { name: 'Borbarad', hp: 30, rs: 1, ini: 20, dead: false },
-  { name: 'Skelett 1', hp: 30, rs: 1, ini: 21, dead: false },
-  { name: 'Eridon', hp: 30, rs: 1, ini: 10, dead: false },
-  { name: 'Akja', hp: 30, rs: 1, ini: 10, dead: false },
-  { name: 'Tharg', hp: 30, rs: 1, ini: 10, dead: false },
-  { name: 'Borbarads Mudda', hp: 30, rs: 1, ini: 20, dead: false }
+  { name: 'Schlange 1', hp: 30, rs: 1, ini: 20, mr1: 7, mr2: 5, dead: false },
+  { name: 'Schlange 2', hp: 30, rs: 1, ini: 20,  mr1: 7, mr2: 5, dead: false },
+  { name: 'Liscom', hp: 30, rs: 1, ini: 20,  mr1: 7, mr2: 5, dead: false },
+  { name: 'Borbarad', hp: 30, rs: 1, ini: 20,  mr1: 7, mr2: 5, dead: false },
+  { name: 'Skelett 1', hp: 30, rs: 1, ini: 21,  mr1: 7, mr2: 5, dead: false },
+  { name: 'Eridon', hp: 30, rs: 1, ini: 10,  mr1: 7, mr2: 5, dead: false },
+  { name: 'Akja', hp: 30, rs: 1, ini: 10,  mr1: 7, mr2: 5, dead: false },
+  { name: 'Tharg', hp: 30, rs: 1, ini: 10,  mr1: 7, mr2: 5, dead: false },
+  { name: 'Borbarads Mudda', hp: 30, rs: 1, ini: 20,  mr1: 7, mr2: 5, dead: false }
 ];
 
 @Component({
@@ -35,13 +38,22 @@ export class AppComponent {
   onTurn = Hero[0];
   isVisible = false;
   iniDamage = 0;
+  ignoreRs = false;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
   dealDamage(damage): void {
-    if (damage > this.selectedHero.rs || damage < 0) {
+    if (this.ignoreRs == true || damage < 0) {
+
+      this.selectedHero.hp -= damage;
+
+      if(this.selectedHero.hp > 0){
+        this.selectedHero.dead = false;
+      }
+      this.ignoreRs = false;
+    } else if (damage > this.selectedHero.rs) {
         this.selectedHero.hp -= (damage - this.selectedHero.rs);
     }
     if(this.selectedHero.hp < 0){
